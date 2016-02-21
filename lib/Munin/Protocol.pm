a8103a985ff401e5bbb522a0bc98548e3a2491dc
Stig Sandbeck Mathisen <ssm@fnord.no>
2016-02-21 17:39:27 +0100

    Add some POD

--- lib/Munin/Protocol.pm
+++ lib/Munin/Protocol.pm
@@ -1,5 +1,159 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+Munin::Protocol - Stateful protocol handler for Munin
+
+=head1 SYNOPSIS
+
+    use Munin::Protocol;
+    my $p   = Munin::Protocol->new();
+
+    p->parse_request("list");
+    p->parse_response("something foo bar zome otherplugin");
+
+    $p->parse_request("config something");
+    $p->parse_response(...);
+
+    $p->parse_request("fetch something");
+    $p->parse_response(...);
+
+    # TODO: At this point, if all went well, $p should contain data for one or
+    # more graphs provided by plugin "something". Add documentation for this.
+
+=head1 DESCRIPTION
+
+Munin::Protocol is a stateful protocol handler for a Munin master-node
+connection.
+
+It will keep the name of the last parsed request, and select the correct
+parser for the response. It will also contain a data structure of the last
+response.
+
+=head2 State
+
+The C<Munin::Protocol> object keeps state.  You probably should not share it
+between connections.
+
+In particular:
+
+=over
+
+=item hostname
+
+The hostname of the munin node will be available as C<< $p->node->hostmame >>
+
+=item capabilities
+
+The capabilities common to the munin master and munin node will be available
+as C<< $p->capabilities >>
+
+=item graphs
+
+The graph data retrieved after a successsful config, (with dirtyconfig), or a
+pair of C<config> and C<fetch> statements. (or C<parse_request> and
+C<parse_response>)
+
+=back
+
+=head2 Capabilities
+
+If the "cap" request and response contains a capability, the protocol state
+will store this.
+
+TODO: Add awesome code example
+
+=over
+
+=item dirtyconfig
+
+If the node and master has successfully negotiated the "dirtyconfig"
+capability, the parser will look for values in the plugin response after
+"config".
+
+Hint: The caller should look for values after each "config", and skip the
+"fetch" step for that plugin if present.
+
+=item multigraph
+
+If the "cap" request and response contains "multigraph", the parser accept a
+multigraph response.
+
+Hint: This means that the list of plugin responses will contain data for
+multiple graphs.
+
+=back
+
+=head1 METHODS
+
+An object of this class represents a connection between a munin master and a
+munin node, on the protocol level. It keeps some state for this, in order to
+successfully send a request, and parse a response.
+
+L<Munin::Protocol> implements the following methods:
+
+=head2 new
+
+    my $p = Munin::Protocol->new();
+
+=head2 parse_request
+
+    my $r = $p->parse_request('cap dirtyconfig multigraph');
+    my $r = $p->parse_request('list');
+    my $r = $p->parse_request('config myplugin');
+    my $r = $p->parse_request('fetch myplugin');
+
+=head2 parse_response
+
+    my $graphs = $p->parse_response('')
+
+=head1 DEPENDENCIES
+
+L<Munin::Protocol> depends on the following modules:
+
+=over
+
+=item Regexp::Grammar
+
+This is the engine of this perl module.
+
+=item Contextual::Return
+
+This seemed like a good idea at the time.
+
+=item Carp
+
+This module is in perl core.
+
+=back
+
+=head1 BUGS AND LIMITATIONS
+
+This module is not finished.  It is under development.  It is barely started.
+The functionality in this documentation may be implemented poorly, or not at
+all.  Everything here is also subject to change until an eventual 1.0 release.
+
+Please report problems to Stig Sandbeck Mathisen <L<ssm@fnord.no>>
+Patches are welcome.
+
+=head1 AUTHOR
+
+Stig Sandbeck Mathisen <ssm@fnord.no>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2016 Stig Sandbeck Mathisen <ssm@fnord.no>.
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.
+
+=cut
+
 package Munin::Protocol;
 use strict;
 use warnings;
@@ -64,6 +218,10 @@ sub parse_request {
     else {
         return ( BOOL {0} );
     }
+}
+
+
+sub parse_response {
 }
 
 sub _build_request {
